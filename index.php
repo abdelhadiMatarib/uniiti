9e59b4908308a50709198ceea2ec82813170f896
Antoine <antoine.rapy@gmail.com>
2013-08-12 18:45:00 +0200

    bug ftploy test

--- index.php
+++ index.php
@@ -30,7 +30,7 @@
             
             <figure>
                 <div class="box_mark">
-                    <div class="box_stars">
+                    <div class="box_stars">test
                         <img src="img/pictos_actions/star.png" title="" alt="" />
                         <img src="img/pictos_actions/star.png" title="" alt="" />
                         <img src="img/pictos_actions/star.png" title="" alt="" />
