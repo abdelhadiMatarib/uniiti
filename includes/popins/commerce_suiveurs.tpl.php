d95dd378996948895831d7023a97dd6467c342b9
cayrelantoine <cayrelantoine@gmail.com>
2013-09-03 19:04:34 +0200

    fix box

---
+++ includes/popins/commerce_suiveurs.tpl.php
@@ -0,0 +1,73 @@
+<?php
+        include_once '../../config/configuration.inc.php';
+        include'../head.php'?>
+
+<div class="suggestioncommerce_wrapper">
+    <div class="popin_close_button"><div class="popin_close_button_img_container"></div></div>
+    <div class="suggestioncommerce_head">
+        <div class="suggestioncommerce_img_container">
+            <img src="<?php echo SITE_URL; ?>/img/pictos_popins/sugg_objet_icon.png" title="" alt="" height="44" width="37" />
+        </div><span class="maintitle">Ils suivent ce commerce</span>
+    </div>  
+    <style>
+        .follower{float:left;width:250px;border-bottom:1px solid #e4e4e4;}
+        .follower_img{float:left;}
+        .follower_content{float:left;padding:7px;background-color:#f0f0f0;}
+        .follower_identity_txt{font-size:0.9em;}
+        span.follower_identity_txt{display:block;}
+        span.follower_identity_txt_avis{font-size:0.9em;font-weight:600;display:inline-block;}
+        span.follower_identity_txt_rang{color:#fabe41;font-size:0.8em;font-weight:600;display:inline-block;}
+    </style>
+    <div class="suggestioncommerce_body">
+        <div class="follower">
+            <div class="follower_img"><img src="<?php echo SITE_URL; ?>/img/avatars/1.png" title="" alt="" /></div>
+            <div class="follower_content">
+                <span class="follower_identity_txt">Jean Pierre F. - Paris 15<sup>ème</sup></span>
+                <span class="follower_identity_txt_avis">85 avis</span>
+                <span class="follower_identity_txt_rang"> 355/3558 Confirmé</span>
+            </div>
+        </div>
+                <div class="follower">
+            <div class="follower_img"><img src="<?php echo SITE_URL; ?>/img/avatars/1.png" title="" alt="" /></div>
+            <div class="follower_content">
+                <span class="follower_identity_txt">Jean Pierre F. - Paris 15<sup>ème</sup></span>
+                <span class="follower_identity_txt_avis">85 avis</span>
+                <span class="follower_identity_txt_rang"> 355/3558 Confirmé</span>
+            </div>
+        </div>
+                <div class="follower">
+            <div class="follower_img"><img src="<?php echo SITE_URL; ?>/img/avatars/1.png" title="" alt="" /></div>
+            <div class="follower_content">
+                <span class="follower_identity_txt">Jean Pierre F. - Paris 15<sup>ème</sup></span>
+                <span class="follower_identity_txt_avis">85 avis</span>
+                <span class="follower_identity_txt_rang"> 355/3558 Confirmé</span>
+            </div>
+        </div>
+                <div class="follower">
+            <div class="follower_img"><img src="<?php echo SITE_URL; ?>/img/avatars/1.png" title="" alt="" /></div>
+            <div class="follower_content">
+                <span class="follower_identity_txt">Jean Pierre F. - Paris 15<sup>ème</sup></span>
+                <span class="follower_identity_txt_avis">85 avis</span>
+                <span class="follower_identity_txt_rang"> 355/3558 Confirmé</span>
+            </div>
+        </div>
+                <div class="follower">
+            <div class="follower_img"><img src="<?php echo SITE_URL; ?>/img/avatars/1.png" title="" alt="" /></div>
+            <div class="follower_content">
+                <span class="follower_identity_txt">Jean Pierre F. - Paris 15<sup>ème</sup></span>
+                <span class="follower_identity_txt_avis">85 avis</span>
+                <span class="follower_identity_txt_rang"> 355/3558 Confirmé</span>
+            </div>
+        </div>
+                <div class="follower">
+            <div class="follower_img"><img src="<?php echo SITE_URL; ?>/img/avatars/1.png" title="" alt="" /></div>
+            <div class="follower_content">
+                <span class="follower_identity_txt">Jean Pierre F. - Paris 15<sup>ème</sup></span>
+                <span class="follower_identity_txt_avis">85 avis</span>
+                <span class="follower_identity_txt_rang"> 355/3558 Confirmé</span>
+            </div>
+        </div>
+    </div>
+
+</div>
+</html>
