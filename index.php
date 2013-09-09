cfb8444634758ccd7763e363d066dc496e6e2413
cayrelantoine <cayrelantoine@gmail.com>
2013-09-09 12:06:17 +0200

    deplacement css de index vers main.css

--- index.php
+++ index.php
@@ -24,35 +24,6 @@ $nbavis     = $result['count_avis'];
 $nbnouveauxinscrits = 0;
 
 ?>
-<style>
-    body{position:relative;margin:0;max-width:100%;max-height:100%}
-    .splash_ombre{background:url('../img/pictos_splash/ombre.png') no-repeat 20px;z-index:99;position:absolute;height:705px;padding:5px;width:554px;margin:auto;top:0;left:0;bottom:0;right:0;}
-    body > img{max-width:100%;min-height:90%;position:absolute;top:0;left:0;z-index:98;}
-    .splash_big_wrapper{position:relative;z-index:99;}
-    .splash_head_wrapper{height:70px;}
-    .splash_head{width:840px;margin:0 auto;}
-    .splash_head_items{margin:10px 0;float:left;text-align:center;width:260px;padding:10px;}
-    .splash_head_items span{text-transform:uppercase;display:block;color:white;text-shadow: 0px 1px 1px #252525;filter: dropshadow(color=#252525, offx=0, offy=1);}
-    .splash_head_items_nbr{font-size:1.8em;font-weight:700;}
-    .splash_body_wrapper{height:95%;}
-    .splash_body{text-align:center;position:absolute;height:270px;padding:5px;width:270px;margin:auto;top:0;left:0;bottom:0;right:0;}
-    .splash_body span{display:inline-block;color:white;margin-top:5px;}
-    .splash_body h1{display:block;margin:14px 0;color:white;font-size:1em;font-weight:300;color:white;}
-    .splash_input_login,.splash_input_password{height:40px;width:185px;padding:5px 5px 5px 35px;background-color:transparent;color:white;font-size:1.1em;}
-    .splash_input_login{border-bottom:1px solid #cac8c3;background:url('../img/pictos_splash/icon_user.png') no-repeat 5px 10px;}
-    .splash_input_password{background:url('../img/pictos_splash/icon_clef.png') no-repeat 5px 15px;}
-    .splash_input_button{background-color:transparent;border:1px dashed white;width:185px;padding:5px 10px;color:white;font-size:1.4em;font-weight:300;margin-top:14px;}
-    .splash_link_create_account{font-weight:600;color:white;}
-    .splash_footer_wrapper{position:relative;}
-    .splash_footer{width:185px;margin:0 auto;}
-    .splash_footer_nav{width:240px;margin:0 auto;margin-top:10px;text-shadow: 0px 1px 1px #252525;filter: dropshadow(color=#252525, offx=0, offy=1);}
-    .splash_footer_nav ul li{display:inline;}
-    .splash_footer_nav ul li a{color:white;}
-    .splash_footer_nav ul span{display:inline;color:white;}
-    .splash_commerce_title{position:absolute;bottom:0;right:20px;}
-    .splash_commerce_title span{color:white;text-shadow: 0px 1px 1px #252525;filter: dropshadow(color=#252525, offx=0, offy=1);}
-    .splash_commerce_title span a{color:white;}
-</style> 
 <body>
     <div id="default_dialog_inscription"></div>
     <div class="splash_ombre"></div>
