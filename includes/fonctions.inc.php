35baf94e44e4f5eb653aa3c37537d586c91565ed
fflandin <francois.flandin@wanadoo.fr>
2013-09-11 17:35:44 +0200

    nouveau

--- includes/fonctions.inc.php
+++ includes/fonctions.inc.php
@@ -63,31 +63,31 @@ function EcartDate($Maintenant, $Date){
 	$Secondes = strtotime($Maintenant) - strtotime($Date);
 	switch ($Secondes) {
 	case ($Secondes < 1800):
-		$html .= "<div class=\"box_posttime\" style=\"background:none;\"><time><span class=\"box_new_item\">nouveau</span></time></div>";
+		$html .= "<div class=\"box_posttime\"><time><span class=\"box_new_item\">nouveau</span></time></div>";
 		break;
 	case ($Secondes < 3600):
 		$Minutes = round($Secondes / 60);
-		$html .= "<div class=\"box_posttime\"><time>Il y a <strong>" . $Minutes . " min.</strong></time></div>";
+		$html .= "<div class=\"box_posttime\"><time><img src=\"img/pictos_actions/clock.png\"/>Il y a <strong>" . $Minutes . " min.</strong></time></div>";
 		break;
 	case ($Secondes < 86400):
 		$Heures = round($Secondes /60 / 60);
-		$html .= "<div class=\"box_posttime\"><time>Il y a <strong>" . $Heures . " h.</strong></time></div>";
+		$html .= "<div class=\"box_posttime\"><time><img src=\"img/pictos_actions/clock.png\"/>Il y a <strong>" . $Heures . " h.</strong></time></div>";
 		break;
 	case ($Secondes < 2592000):
 		$Heures = round($Secondes /60 / 60 /24);
-		$html .= "<div class=\"box_posttime\"><time>Il y a <strong>" . $Heures . " j.</strong></time></div>";
+		$html .= "<div class=\"box_posttime\"><time><img src=\"img/pictos_actions/clock.png\"/>Il y a <strong>" . $Heures . " j.</strong></time></div>";
 		break;	
 	case ($Secondes < 31104000):
 		$Heures = round($Secondes /60 / 60 /24 /30);
-		$html .= "<div class=\"box_posttime\"><time>Il y a <strong>" . $Heures . " mois</strong></time></div>";
+		$html .= "<div class=\"box_posttime\"><time><img src=\"img/pictos_actions/clock.png\"/>Il y a <strong>" . $Heures . " mois</strong></time></div>";
 		break;
 	case ($Secondes < 746496000):
 		$Heures = round($Secondes /60 / 60 /24 /30 /12);
-		$html .= "<div class=\"box_posttime\"><time>Il y a <strong>" . $Heures . " an</strong></time></div>";
+		$html .= "<div class=\"box_posttime\"><time><img src=\"img/pictos_actions/clock.png\"/>Il y a <strong>" . $Heures . " an</strong></time></div>";
 		break;				
 	default :
 		$Jours = round($Secondes /60 / 60 /24 /30 /12 );
-		$html .= "<div class=\"box_posttime\"><time>Il y a <strong>" . $Jours . " ans</strong></time></div>";
+		$html .= "<div class=\"box_posttime\"><time><img src=\"img/pictos_actions/clock.png\"/>Il y a <strong>" . $Jours . " ans</strong></time></div>";
 		break;			
 	}
 	return $html;
