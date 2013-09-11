5d4a2a7d8074ea38e664a6cc4b584dba992833df
cayrelantoine <cayrelantoine@gmail.com>
2013-09-11 17:36:58 +0200

    fix box_posttype

---
+++ includes/popins/dashboard_gestion_commerce_notifications.tpl.php
@@ -0,0 +1,76 @@
+<?php 
+	include_once '../../acces/auth.inc.php';                 // Gestion accès à la page - incluant la session	
+	include_once '../../config/configuration.inc.php';
+	include_once '../../config/configPDO.inc.php';
+?>                      
+                        <!-- ITEM -->
+                        <div class="dashboard_notif_item">
+                            <div class="dashboard_notif_item_head">
+                                <div class="dashboard_notif_item_head_img_container">
+                                    <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/photo_notif.jpg"/>
+                                </div>
+                                <div class="dashboard_notif_item_head_desc">
+                                    <span class="dashboard_notif_nom_commerce">Restaurant Chez les Artistes</span>
+                                    <span class="dashboard_notif_motif">Demande de modification du profil</span>
+                                    <span class="dashboard_notif_temps">Il y a 2 jours</span>
+                                </div>
+                                <div class="dashboard_notif_item_head_buttons">
+                                    <a href="#" class="first_img_margin" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/></a>
+                                    <a href="#" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/></a>
+                                </div>
+                            </div>
+                            <div class="dashboard_notif_item_body">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus sed est sit amet laoreet. Donec enim neque, cursus vitae euismod at, faucibus nec neque. Etiam diam magna, ultrices ac commodo a, sodales non odio. Aliquam vel aliquet libero. Ut pellentesque odio nibh, eget euismod magna mollis quis. Sed lectus leo, mollis in sollicitudin quis, egestas a lorem.</p>
+                            </div>
+                        </div>
+                        <!-- ITEM -->
+                        
+                        <!-- ITEM -->
+                        <div class="dashboard_notif_item">
+                            <div class="dashboard_notif_item_head">
+                                <div class="dashboard_notif_item_head_img_container">
+                                    <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/photo_notif.jpg"/>
+                                </div>
+                                <div class="dashboard_notif_item_head_desc">
+                                    <span class="dashboard_notif_nom_commerce">Restaurant Positano</span>
+                                    <span class="dashboard_notif_motif">Demande de suppression d'un avis</span>
+                                    <span class="dashboard_notif_temps">Il y a 4 jours</span>
+                                </div>
+                                <div class="dashboard_notif_item_head_buttons">
+                                    <a href="#" class="first_img_margin" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/></a>
+                                    <a href="#" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/></a>
+                                </div>
+                            </div>
+                            <div class="dashboard_notif_item_body">
+                                <div class="dashboard_notif_item_body_small_head">
+                                <span class="dashboard_notif_txt_normal">Commentaire laissé par Arnaud K.</span><span class="dashboard_notif_txt_bold">Motif de suppression : Avis à caractère diffamatoire</span>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus sed est sit amet laoreet. Donec enim neque, cursus vitae euismod at, faucibus nec neque. Etiam diam magna, ultrices ac commodo a, sodales non odio. Aliquam vel aliquet libero. Ut pellentesque odio nibh, eget euismod magna mollis quis. Sed lectus leo, mollis in sollicitudin quis, egestas a lorem.</p>
+                            </div>
+                        </div>
+                        <!-- ITEM -->
+                        
+                        <!-- ITEM -->
+                        <div class="dashboard_notif_item">
+                            <div class="dashboard_notif_item_head">
+                                <div class="dashboard_notif_item_head_img_container">
+                                    <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/photo_notif.jpg"/>
+                                </div>
+                                <div class="dashboard_notif_item_head_desc">
+                                    <span class="dashboard_notif_nom_commerce">Restaurant Positano</span>
+                                    <span class="dashboard_notif_motif">Demande de suppression d'un avis</span>
+                                    <span class="dashboard_notif_temps">Il y a 4 jours</span>
+                                </div>
+                                <div class="dashboard_notif_item_head_buttons">
+                                    <a href="#" class="first_img_margin" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/></a>
+                                    <a href="#" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/></a>
+                                </div>
+                            </div>
+                            <div class="dashboard_notif_item_body">
+                                <div class="dashboard_notif_item_body_small_head">
+                                <span class="dashboard_notif_txt_normal">Commentaire laissé par Arnaud K.</span><span class="dashboard_notif_txt_bold">Motif de suppression : Avis à caractère diffamatoire</span>
+                                </div>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus sed est sit amet laoreet. Donec enim neque, cursus vitae euismod at, faucibus nec neque. Etiam diam magna, ultrices ac commodo a, sodales non odio. Aliquam vel aliquet libero. Ut pellentesque odio nibh, eget euismod magna mollis quis. Sed lectus leo, mollis in sollicitudin quis, egestas a lorem.</p>
+                            </div>
+                        </div>
+                        <!-- ITEM -->                
--- pages/dashboard_gestion_commerce.php
+++ pages/dashboard_gestion_commerce.php
@@ -64,77 +64,16 @@
             </div>
             <div class="dashboard_ombre_small"><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/ombre_cube_small.png"/></div>
             <div class="dashboard_content gestion_content">
-                <div class="dashboard_center_title"><h2><a href="#" title="">Notifications commerces </a></h2><h2>|<a href="#" title=""> Suggestions commerces</a></h2></div>
+                <div class="dashboard_center_title"><h2><a href="#" title="" class="load_notif_commerce">Notifications commerces </a></h2><h2>|<a href="#" title="" class="load_sugg_commerce"> Suggestions commerces</a></h2></div>
                     <div class="dashboard_notif_wrap">
-                        <!-- ITEM -->
-                        <div class="dashboard_notif_item">
-                            <div class="dashboard_notif_item_head">
-                                <div class="dashboard_notif_item_head_img_container">
-                                    <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/photo_notif.jpg"/>
-                                </div>
-                                <div class="dashboard_notif_item_head_desc">
-                                    <span class="dashboard_notif_nom_commerce">Restaurant Chez les Artistes</span>
-                                    <span class="dashboard_notif_motif">Demande de modification du profil</span>
-                                    <span class="dashboard_notif_temps">Il y a 2 jours</span>
-                                </div>
-                                <div class="dashboard_notif_item_head_buttons">
-                                    <a href="#" class="first_img_margin" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/></a>
-                                    <a href="#" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/></a>
-                                </div>
-                            </div>
-                            <div class="dashboard_notif_item_body">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus sed est sit amet laoreet. Donec enim neque, cursus vitae euismod at, faucibus nec neque. Etiam diam magna, ultrices ac commodo a, sodales non odio. Aliquam vel aliquet libero. Ut pellentesque odio nibh, eget euismod magna mollis quis. Sed lectus leo, mollis in sollicitudin quis, egestas a lorem.</p>
-                            </div>
+                        
+                        <div class="dashboard_load_notifs">
+                        
+                        
+                        
                         </div>
-                        <!-- ITEM -->
-                                                <!-- ITEM -->
-                        <div class="dashboard_notif_item">
-                            <div class="dashboard_notif_item_head">
-                                <div class="dashboard_notif_item_head_img_container">
-                                    <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/photo_notif.jpg"/>
-                                </div>
-                                <div class="dashboard_notif_item_head_desc">
-                                    <span class="dashboard_notif_nom_commerce">Restaurant Positano</span>
-                                    <span class="dashboard_notif_motif">Demande de suppression d'un avis</span>
-                                    <span class="dashboard_notif_temps">Il y a 4 jours</span>
-                                </div>
-                                <div class="dashboard_notif_item_head_buttons">
-                                    <a href="#" class="first_img_margin" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/></a>
-                                    <a href="#" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/></a>
-                                </div>
-                            </div>
-                            <div class="dashboard_notif_item_body">
-                                <div class="dashboard_notif_item_body_small_head">
-                                <span class="dashboard_notif_txt_normal">Commentaire laissé par Arnaud K.</span><span class="dashboard_notif_txt_bold">Motif de suppression : Avis à caractère diffamatoire</span>
-                                </div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus sed est sit amet laoreet. Donec enim neque, cursus vitae euismod at, faucibus nec neque. Etiam diam magna, ultrices ac commodo a, sodales non odio. Aliquam vel aliquet libero. Ut pellentesque odio nibh, eget euismod magna mollis quis. Sed lectus leo, mollis in sollicitudin quis, egestas a lorem.</p>
-                            </div>
-                        </div>
-                        <!-- ITEM -->
-                                                                   <!-- ITEM -->
-                        <div class="dashboard_notif_item">
-                            <div class="dashboard_notif_item_head">
-                                <div class="dashboard_notif_item_head_img_container">
-                                    <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/photo_notif.jpg"/>
-                                </div>
-                                <div class="dashboard_notif_item_head_desc">
-                                    <span class="dashboard_notif_nom_commerce">Restaurant Positano</span>
-                                    <span class="dashboard_notif_motif">Demande de suppression d'un avis</span>
-                                    <span class="dashboard_notif_temps">Il y a 4 jours</span>
-                                </div>
-                                <div class="dashboard_notif_item_head_buttons">
-                                    <a href="#" class="first_img_margin" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/></a>
-                                    <a href="#" title=""><img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/></a>
-                                </div>
-                            </div>
-                            <div class="dashboard_notif_item_body">
-                                <div class="dashboard_notif_item_body_small_head">
-                                <span class="dashboard_notif_txt_normal">Commentaire laissé par Arnaud K.</span><span class="dashboard_notif_txt_bold">Motif de suppression : Avis à caractère diffamatoire</span>
-                                </div>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus sed est sit amet laoreet. Donec enim neque, cursus vitae euismod at, faucibus nec neque. Etiam diam magna, ultrices ac commodo a, sodales non odio. Aliquam vel aliquet libero. Ut pellentesque odio nibh, eget euismod magna mollis quis. Sed lectus leo, mollis in sollicitudin quis, egestas a lorem.</p>
-                            </div>
-                        </div>
-                        <!-- ITEM -->
+                        <!-- DASHBOARD LOAD NOTIFS -->
+                        
                         <h2 class="h2_suppr_commerce">Suppression d'un commerce</h2>
                         <div class="suppression_commerce_wrap">
                         <span class="suppression_warning">Attention cette action est définitive</span>
@@ -156,6 +95,16 @@
     document.getElementById(nextFieldID).focus();
   }
 }
+</script>
+<script>
+$('.load_sugg_commerce').click(function(e){
+    e.preventDefault();
+   $('.dashboard_load_notifs').load('../includes/popins/dashboard_gestion_commerce_suggestions.tpl.php').slideUp().slideDown();
+});
+$('.load_notif_commerce').click(function(e){
+    e.preventDefault();
+   $('.dashboard_load_notifs').load('../includes/popins/dashboard_gestion_commerce_notifications.tpl.php').slideUp().slideDown();
+});
 </script>
 
     </body>
