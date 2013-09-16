69e37f25c8b355e8c2e2e88f1d2ca24a70c179d2
Antoine <antoine.rapy@gmail.com>
2013-09-16 18:28:05 +0200

    config

---
+++ config/configPDO.inc.php
@@ -0,0 +1,34 @@
+<?php
+$db_config = array();
+$db_config['DRIVER']        = 'mysql';
+$db_config['HOST']          = 'localhost';
+$db_config['DB_NAME']       = 'captainopinionDB';
+$db_config['USER']          = 'root';                               // LOCAL//
+//$db_config['USER']        = 'captainJohanUser';                   // SERVEUR
+//$db_config['PASSWORD']      = 'root';                               // LOCAL
+$db_config['PASSWORD']    = '';                // SERVEUR
+$db_config['OPTIONS']       = array(
+                                // Activation des exceptions PDO :
+                                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                                //PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
+                                );
+
+try
+{
+    // Connexion à la base
+    $bdd = new PDO($db_config['DRIVER'] .':host='. $db_config['HOST'] .';dbname='. $db_config['DB_NAME'],
+                   $db_config['USER'],
+                   $db_config['PASSWORD'],
+                   $db_config['OPTIONS']
+                   );
+    
+    $bdd->exec("SET NAMES 'utf8'");
+}
+catch(Exception $e)
+{
+    echo 'Erreur : ' . $e->getMessage();
+    die();
+}
+
+
+?>   
---
+++ config/configuration.inc.php
@@ -0,0 +1,6 @@
+<?php
+//define ("SITE_URL", "http://localhost/uniiti/uniiti");       
+define ("SITE_URL", "http://127.0.0.1/uniiti");
+//define ("SITE_URL", "http://www.captainopinion.bullegom-server.com/captain-opinion");                    // Constante URL SERVEUR
+//define ("SITE_URL", "http://cluster015.ovh.net/~captaino/uniiti");
+?>
