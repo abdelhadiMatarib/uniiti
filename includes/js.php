187d52f4a4b93cc6962f96bac51b45d373cd1041
Antoine <antoine.rapy@gmail.com>
2013-09-18 21:55:14 +0200

    commit du soir

--- includes/js.php
+++ includes/js.php
@@ -10,6 +10,7 @@
 	<script src="<?php echo SITE_URL; ?>/js/jquery.easydrag.dialog.min.js"></script>
 	<script src="<?php echo SITE_URL; ?>/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 	<script src="<?php echo SITE_URL; ?>/js/jquery.slides.min.js"></script>
+	
 	<script>
 	
 	$Filtre = {};
--- js/main.js
+++ js/main.js
@@ -119,19 +119,37 @@ function CreerOverlayPush() {
        $('.overlay_social_buttons').hide().fadeIn().toggleClass('overlay_social_buttons_specific');
        $('.overlay_social_buttons_specific img').css('margin-right','5px');
     });
-    
+    var Inittop = 0;
+    var top2;
+
         // RESIZING THE WRAPPER DEPENDING ON BOXES CONTAINER
-function resizeboxContainer(){        
+function resizeboxContainer(){
 	var boxcontainersize = $('#box_container').width();
-        var biggymarginer = $('.biggymarginer').width();
+    var biggymarginer = $('.biggymarginer').width();
+    var couv2 = $('#couv2');
+    var height = $('#couv2').height();
+    if (Inittop == 0) {
+	    var top2px = couv2.css('marginTop');
+	    heightcouv2 = couv2.height();
+	    top2 = Math.round(top2px.substring(0,top2px.length-2));
+	 	Inittop = 1;
+    }
+    var top1resize = (top2*height)/heightcouv2;
+    var top2resize = (top2*height)/heightcouv2;
+    var top3resize = (top2*height)/heightcouv2;
+    var top4resize = (top2*height)/heightcouv2;
+    var top5resize = (top2*height)/heightcouv2;
+
+    console.log(top5resize);
+
 
 	if (boxcontainersize == 250){$('.big_wrapper').css('width',236);}
 	if (boxcontainersize == 500){$('.big_wrapper').css('width',486);}
-	if (boxcontainersize == 750){$('.big_wrapper').css('width',736);$('.commerce_couv').css('height',210);$(".slidesjs-control").css('width' ,736);$(".slidesjs-control").css('width' ,736);$(".slidesjs-control").css('height' ,210);}
-	if (boxcontainersize == 1000){$('.big_wrapper').css('width',986);$('.commerce_couv').css('height',282);$(".slidesjs-control").css('width' ,986);$(".slidesjs-control").css('width' ,986);$(".slidesjs-control").css('height' ,282);}
-	if (boxcontainersize == 1250){$('.big_wrapper').css('width',1236);$('.commerce_couv').css('height',353);$(".slidesjs-control").css('width' ,1236);$(".slidesjs-control").css('width' ,1236);$(".slidesjs-control").css('height' ,353);}
-	if (boxcontainersize == 1500){$('.big_wrapper').css('width',1486);$('.commerce_couv').css('height',425);$(".slidesjs-control").css('width' ,1486);$(".slidesjs-control").css('width' ,1486);$(".slidesjs-control").css('height' ,425);}
-	if (boxcontainersize == 1750){$('.big_wrapper').css('width',1736);$('.commerce_couv').css('height',496);$(".slidesjs-control").css('width' ,1736);$(".slidesjs-control").css('width' ,1736);$(".slidesjs-control").css('height' ,496);}
+	if (boxcontainersize == 750){$('.big_wrapper').css('width',736);$('.commerce_couv').css('height',210);couv2.css({"marginTop": top1resize+"px"});/*$(".slidesjs-container").css('width' ,736);*/$(".slidesjs2-control").css('width' ,736);/*$(".slidesjs-control").css('height' ,210);*/}
+	if (boxcontainersize == 1000){$('.big_wrapper').css('width',986);$('.commerce_couv').css('height',282);couv2.css({"marginTop": top2resize+"px"});/*$(".slidesjs-container").css('width' ,986);*/$(".slidesjs2-control").css('width' ,986);/*$(".slidesjs-control").css('height' ,282);*/}
+	if (boxcontainersize == 1250){$('.big_wrapper').css('width',1236);$('.commerce_couv').css('height',353);couv2.css({"marginTop": top3resize+"px"});/*$(".slidesjs-container").css('width' ,1236);*/$(".slidesjs2-control").css('width' ,1236);/*$(".slidesjs-control").css('height' ,353);*/}
+	if (boxcontainersize == 1500){$('.big_wrapper').css('width',1486);$('.commerce_couv').css('height',425);couv2.css({"marginTop": top4resize+"px"});/*$(".slidesjs-container").css('width' ,1486);*/$(".slidesjs2-control").css('width' ,1486);/*$(".slidesjs-control").css('height' ,425);*/}
+	if (boxcontainersize == 1750){$('.big_wrapper').css('width',1736);$('.commerce_couv').css('height',496);couv2.css({"marginTop": top5resize+"px"});/*$(".slidesjs-container").css('width' ,1736);*/$(".slidesjs2-control").css('width' ,1736);/*$(".slidesjs-control").css('height' ,496);*/}
 	if (boxcontainersize == 2000){$('.big_wrapper').css('width',1986);}
 	
 	// RESIZING CONTENT DEPENDING ON SIZES
