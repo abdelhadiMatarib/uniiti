aaff1330e7b255a6a33cf29ed3163bc6dc335275
fflandin <francois.flandin@wanadoo.fr>
2013-09-23 11:43:59 +0200

    problème de cache des images de box + annulation hover box

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 function AfficheImage(img) {
 	img.fadeIn(1000);
-}
+}	
 
 $("a").click(function(e) {
 	if (($("#loading_page").length > 0) && ($(this).attr("href") != "#")) {
@@ -128,7 +128,7 @@ function OuvrePopinMotDePasseOublie(data) {
 
 function CreerOverlayPush() {
     // Push image box
-/*    $('.box_push_et_img').click(function(e) {
+    $('.box_push_et_img').click(function(e) {
         e.preventDefault();//don't go to default URL
 		var overlay_push = $(this).next('.overlay_push');
 		overlay_push.click(function(e){
@@ -137,7 +137,13 @@ function CreerOverlayPush() {
 		});
 		$('.overlay_push').css('display','none');
 		overlay_push.css('display','block');
-    });	*/
+    });	
+	$(".box_push_et_img img").each(function () {$(this).load(function () {$(this).fadeIn(1000);});});
+/*	$(".box section").each(function () {$(this).click(function () {				////////////////// ATTENTION ATTENTION GESTION INTERNET EXPLORER
+			eval($(this).attr('onclick'));
+		});
+	}); */
+//	$(".presentation_action_left_figure img").each(function () {$(this).load(function () {alert($(this).attr('src'));$(this).fadeIn(1000);});});
 	
 	$(".box_useraction a").click(function(e) {
 		e.preventDefault(); //don't go to default URL
@@ -147,11 +153,11 @@ function CreerOverlayPush() {
 	});
 
 /////// DEBUT TEST
-    $('.box_push_et_img').mouseenter(function(e) {
+/*    $('.box_push_et_img').mouseenter(function(e) {
 		var overlay_push = $(this).next('.overlay_push');
 		overlay_push.css('display','block');
 		overlay_push.mouseleave(function(e) {$(this).css('display','none');});
-    });
+    }); */
 
 /////// FIN TEST
 }
