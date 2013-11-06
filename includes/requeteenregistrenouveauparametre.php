8411d262a9e4c3e87f64699373f57ba5ba73ab24
fflandin <francois.flandin@wanadoo.fr>
2013-11-06 14:59:21 +0100

    Modif et suppression paramètres

--- includes/requeteenregistrenouveauparametre.php
+++ includes/requeteenregistrenouveauparametre.php
@@ -5,22 +5,56 @@ include_once '../config/configPDO.inc.php';
 if (isset($_POST['type'])) {$type = $_POST['type'];} else {exit;}
 if (isset($_POST['table'])) {$table = $_POST['table'];} else {exit;}
 if (isset($_POST['valeurs'])) {$valeurs = htmlspecialchars($_POST['valeurs']);} else {exit;}
+if (isset($_POST['modif'])) {$modif = htmlspecialchars($_POST['modif']);}
 
 switch ($table) {
 	case "recommandations" :
 		$parametre1 = "recommandation";
+		$id = "id_recommandation";
+		$sqlcheck2 = "SELECT * FROM enseignes_recommandations AS t1";
+		$sqlcheck2 .= " INNER JOIN recommandations AS t2";
+		$sqlcheck2 .= " ON t1.id_recommandation = t2.id_recommandation";
+		$sqlcheck2 .= " INNER JOIN enseignes AS t3";
+		$sqlcheck2 .= " ON t1.enseignes_id_enseigne = t3.id_enseigne";
+		$sqlcheck2 .= " WHERE t2.recommandation =:valeurs";
 		break;
 	case "labelsuniiti" :
 		$parametre1 = "label";
+		$id = "id_label";
+		$sqlcheck2 = "SELECT * FROM enseignes_labelsuniiti AS t1";
+		$sqlcheck2 .= " INNER JOIN labelsuniiti AS t2";
+		$sqlcheck2 .= " ON t1.id_label = t2.id_label";
+		$sqlcheck2 .= " INNER JOIN enseignes AS t3";
+		$sqlcheck2 .= " ON t1.enseignes_id_enseigne = t3.id_enseigne";
+		$sqlcheck2 .= " WHERE t2.label =:valeurs";
 		break;
 	case "motscles" :
 		$parametre1 = "motcle";
+		$id = "id_motcle";
+		$sqlcheck2 = "SELECT * FROM enseignes_infos_generales AS t1";
+		$sqlcheck2 .= " INNER JOIN motscles AS t2";
+		$sqlcheck2 .= " ON (t1.id_motcle1 = t2.id_motcle OR t1.id_motcle2 = t2.id_motcle OR t1.id_motcle3 = t2.id_motcle)";
+		$sqlcheck2 .= " INNER JOIN enseignes AS t3";
+		$sqlcheck2 .= " ON t1.enseignes_id_enseigne = t3.id_enseigne";
+		$sqlcheck2 .= " WHERE t2.motcle =:valeurs";
 		break;
 	case "moyenspaiements" :
 		$parametre1 = "moyenpaiement";
+		$id = "id_moyenpaiement";
+		$sqlcheck2 = "SELECT * FROM enseignes_moyenspaiements AS t1";
+		$sqlcheck2 .= " INNER JOIN moyenspaiements AS t2";
+		$sqlcheck2 .= " ON t1.id_moyenpaiement = t2.id_moyenpaiement";
+		$sqlcheck2 .= " INNER JOIN enseignes AS t3";
+		$sqlcheck2 .= " ON t1.enseignes_id_enseigne = t3.id_enseigne";
+		$sqlcheck2 .= " WHERE t2.moyenpaiement =:valeurs";
 		break;
 	case "villes" :
 		$parametre1 = "nom_ville";
+		$id = "id_ville";
+		$sqlcheck2 = "SELECT * FROM enseignes AS t1";
+		$sqlcheck2 .= " INNER JOIN villes AS t2";
+		$sqlcheck2 .= " ON t1.villes_id_ville = t2.id_ville";
+		$sqlcheck2 .= " WHERE t2.nom_ville =:valeurs";
 		break;
 	default:
 		exit;
@@ -32,16 +66,64 @@ try
 	// Requete
 	$bdd->beginTransaction(); // Début transaction pour requetes multiples
 
-	$sql = "INSERT INTO " . $table . " (" . $parametre1 . ") VALUES (:valeurs)";
-	$req = $bdd->prepare($sql);
-	$req->bindParam(':valeurs', $valeurs, PDO::PARAM_STR);
-	$req->execute();
+	$sqlcheck = "SELECT * FROM " . $table . " WHERE " . $parametre1 . " =:valeurs";
+	$reqcheck = $bdd->prepare($sqlcheck);
+	$reqcheck->bindParam(':valeurs', $valeurs, PDO::PARAM_STR);
+	$reqcheck->execute();
+	$resultcheck = $reqcheck->fetch(PDO::FETCH_ASSOC);				
+	if ((!$resultcheck) && ($type == "enregistrer")) {
+	
+		$sql = "INSERT INTO " . $table . " (" . $parametre1 . ") VALUES (:valeurs)";
+		$req = $bdd->prepare($sql);
+		$req->bindParam(':valeurs', $valeurs, PDO::PARAM_STR);
+		$req->execute();
 
-	$nouvel_id = $bdd->lastInsertId();
-	$bdd->commit(); // Validation de la transaction / des requetes
-	$req->closeCursor();
+		$nouvel_id = $bdd->lastInsertId();
+		$bdd->commit(); // Validation de la transaction / des requetes
+		$req->closeCursor();
 
-	$data['result'] = $nouvel_id;
+		$data['result'] = $nouvel_id;
+	} else if (($resultcheck) && ($type == "supprimer")) {
+	
+		$reqcheck2 = $bdd->prepare($sqlcheck2);
+		$reqcheck2->bindParam(':valeurs', $valeurs, PDO::PARAM_STR);
+		$reqcheck2->execute();
+		$resultcheck2 = $reqcheck2->fetchAll(PDO::FETCH_ASSOC);		
+	
+		if (!$resultcheck2) {
+			$sql = "DELETE FROM " . $table . " WHERE " . $parametre1 . "= :valeurs";
+			$req = $bdd->prepare($sql);
+			$req->bindParam(':valeurs', $valeurs, PDO::PARAM_STR);
+			$req->execute();
+
+			$bdd->commit(); // Validation de la transaction / des requetes
+			$req->closeCursor();
+
+			$data['result'] = 1;
+		} else {
+			$data['result'] = 0;
+			foreach ($resultcheck2 as $row) {
+				$data['id'][$row['id_enseigne']] = $row['nom_enseigne'];
+			}
+		}
+	} else 	if (($resultcheck) && ($type == "modifier")) {
+	
+		$sql = "UPDATE " . $table . " SET " . $parametre1 . "=:modif" . " WHERE " . $parametre1 . "=:valeurs";
+		$req = $bdd->prepare($sql);
+		$req->bindParam(':modif', $modif, PDO::PARAM_STR);
+		$req->bindParam(':valeurs', $valeurs, PDO::PARAM_STR);
+		$req->execute();
+
+		$bdd->commit(); // Validation de la transaction / des requetes
+		$req->closeCursor();
+
+		$data['result'] = 1;
+	} else if (($resultcheck) && ($type == "enregistrer")) {
+		$data['result'] = -1;
+		$data['id'] = $resultcheck[$id];
+	} else if ((!$resultcheck) && (($type == "supprimer") || ($type == "modifier"))) {
+		$data['result'] = -1;
+	}
 
 	$bdd = null;            // Détruit l'objet PDO
 	
--- pages/dashboard_parametres.php
+++ pages/dashboard_parametres.php
@@ -13,6 +13,56 @@
 
 	if ((isset($_SESSION['SESS_MEMBER_ID'])) && ((int)$_SESSION['droits'] & ADMINISTRATEUR)) {$Connecte = true;}
 	else {echo "vous ne pouvez pas accéder à cette page sans être connecté!\n<a href=\"" . SITE_URL . "\">Revenir à la page principale</a>"; exit;}
+
+	$sql1 = "SELECT * FROM villes";
+	$req1 = $bdd->prepare($sql1);
+	$req1->execute();	
+	$result1 = $req1->fetchAll(PDO::FETCH_ASSOC);
+
+	$sql2 = "SELECT * FROM recommandations";
+	$req2 = $bdd->prepare($sql2);
+	$req2->execute();	
+	$result2 = $req2->fetchAll(PDO::FETCH_ASSOC);
+
+	$sql3 = "SELECT * FROM labelsuniiti";
+	$req3 = $bdd->prepare($sql3);
+	$req3->execute();	
+	$result3 = $req3->fetchAll(PDO::FETCH_ASSOC);
+
+	$sql4 = "SELECT * FROM motscles";
+	$req4 = $bdd->prepare($sql4);
+	$req4->execute();	
+	$result4 = $req4->fetchAll(PDO::FETCH_ASSOC);
+
+	$sql5 = "SELECT * FROM moyenspaiements";
+	$req5 = $bdd->prepare($sql5);
+	$req5->execute();	
+	$result5 = $req5->fetchAll(PDO::FETCH_ASSOC);
+
+	$sql6 = "SELECT * FROM categories";
+	$req6 = $bdd->prepare($sql6);
+	$req6->execute();	
+	$result6 = $req6->fetchAll(PDO::FETCH_ASSOC);
+
+	$sql7 = "SELECT * FROM sous_categories";
+	$req7 = $bdd->prepare($sql7);
+	$req7->execute();	
+	$result7 = $req7->fetchAll(PDO::FETCH_ASSOC);	
+
+	$sql8 = "SELECT * FROM sous_categories2";
+	$req8 = $bdd->prepare($sql8);
+	$req8->execute();	
+	$result8 = $req8->fetchAll(PDO::FETCH_ASSOC);	
+
+	$sql9 = "SELECT * FROM arrondissement";
+	$req9 = $bdd->prepare($sql9);
+	$req9->execute();	
+	$result9 = $req9->fetchAll(PDO::FETCH_ASSOC);	
+
+	$sql10 = "SELECT * FROM quartier";
+	$req10 = $bdd->prepare($sql10);
+	$req10->execute();	
+	$result10 = $req10->fetchAll(PDO::FETCH_ASSOC);		
 ?>
 
     <body class="dashboard">
@@ -43,64 +93,133 @@
             <div class="dashboard_content parametres_content">
                 <h2>Paramètres Uniiti.com </h2>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result6 as $row) { ?>
+						<option value="<?php echo $row['categorie_principale']; ?>"><?php echo $row['categorie_principale']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="categories" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UNE CATÉGORIE"/>
                     <div class="input_dashboard_large_container">
                          <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result7 as $row) { ?>
+						<option value="<?php echo $row['sous_categorie']; ?>"><?php echo $row['sous_categorie']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="sous_categories" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UNE SOUS-CATÉGORIE"/>
                     <div class="input_dashboard_large_container">
                          <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result8 as $row) { ?>
+						<option value="<?php echo $row['sous_categorie2']; ?>"><?php echo $row['sous_categorie2']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="sous_categories2" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UNE SOUS-SOUS CATÉGORIE"/>
                     <div class="input_dashboard_large_container">
                          <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="separation_input_parametres"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result1 as $row) { ?>
+						<option value="<?php echo $row['nom_ville']; ?>"><?php echo $row['nom_ville']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="villes" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UNE VILLE"/>
-                    <div class="input_dashboard_large_container">
-                         <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
+					<div class="input_dashboard_large_container">
+                        <img class="modifier" title="modifier" width="30px" src="<?php echo SITE_URL; ?>/img/pictos_utilisateurs/interface_crayon_icon_n.png"/>
+						<img class="supprimer" title="supprimer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/>
+						<img class="enregistrer" title="enregistrer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result9 as $row) { ?>
+						<option value="<?php echo $row['arrondissement']; ?>"><?php echo $row['arrondissement']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="arrondissement" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UN ARRONDISSEMENT"/>
                     <div class="input_dashboard_large_container">
                          <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result10 as $row) { ?>
+						<option value="<?php echo $row['quartier']; ?>"><?php echo $row['quartier']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="quartier" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UN QUARTIER"/>
                     <div class="input_dashboard_large_container">
                          <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result2 as $row) { ?>
+						<option value="<?php echo $row['recommandation']; ?>"><?php echo $row['recommandation']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="recommandations" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UNE RECOMMANDATION"/>
                     <div class="input_dashboard_large_container">
-                         <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
+                        <img class="modifier" title="modifier" width="30px" src="<?php echo SITE_URL; ?>/img/pictos_utilisateurs/interface_crayon_icon_n.png"/>
+						<img class="supprimer" title="supprimer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/>
+						<img class="enregistrer" title="enregistrer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result3 as $row) { ?>
+						<option value="<?php echo $row['label']; ?>"><?php echo $row['label']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="labelsuniiti" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UN LABEL"/>
                     <div class="input_dashboard_large_container">
-                         <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
+                        <img class="modifier" title="modifier" width="30px" src="<?php echo SITE_URL; ?>/img/pictos_utilisateurs/interface_crayon_icon_n.png"/>
+						<img class="supprimer" title="supprimer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/>
+						<img class="enregistrer" title="enregistrer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result4 as $row) { ?>
+						<option value="<?php echo $row['motcle']; ?>"><?php echo $row['motcle']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="motscles" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UN MOT CLE"/>
                     <div class="input_dashboard_large_container">
-                         <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
+                        <img class="modifier" title="modifier" width="30px" src="<?php echo SITE_URL; ?>/img/pictos_utilisateurs/interface_crayon_icon_n.png"/>
+						<img class="supprimer" title="supprimer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/>
+						<img class="enregistrer" title="enregistrer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
+				<div style="clear:both"></div>
                 <div class="input_dashboard_parametres_wrap">
+					<select disabled="disabled" class="input_dashboard_large_parametres">
+						<?php foreach ($result5 as $row) { ?>
+						<option value="<?php echo $row['moyenpaiement']; ?>"><?php echo $row['moyenpaiement']; ?></option>
+						<?php } ?>
+					</select>
                     <input id="moyenspaiements" class="input_dashboard_large_parametres" type="text" placeholder="AJOUTER UN MOYEN DE PAIEMENT"/>
                     <div class="input_dashboard_large_container">
-                         <img src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
+                        <img class="modifier" title="modifier" width="30px" src="<?php echo SITE_URL; ?>/img/pictos_utilisateurs/interface_crayon_icon_n.png"/>
+						<img class="supprimer" title="supprimer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_suppr.png"/>
+						<img class="enregistrer" title="enregistrer" src="<?php echo SITE_URL; ?>/img/pictos_dashboard/bouton_notif_valide.png"/>
                     </div>
                 </div>
 <!-- 
@@ -134,11 +253,23 @@
 		if(field.value.length >= field.maxLength){document.getElementById(nextFieldID).focus();}
 	}
 	
-	$(".input_dashboard_large_container").click(function () {
-		var input = $(this).prev('input');
+	$(".enregistrer").click(function () {
+		var input = $(this).parent().prev('input');
+		var placeholder = input.attr('placeholder');
+		var select = $(this).parent().prev().prev();
+		if ((placeholder.lastIndexOf("MODIFIER") != -1) || ((placeholder.lastIndexOf("SUPPRIMER") != -1))){
+			select.attr('disabled','disabled');
+			input.removeAttr('disabled');
+			input.val('');
+			placeholder = placeholder.replace("MODIFIER","AJOUTER");
+			placeholder = placeholder.replace("SUPPRIMER","AJOUTER");
+			input.attr('placeholder',placeholder);
+			return false;
+		}
 		var valeurs = input.val();
 		var table = input.attr('id');
-	
+		if (valeurs == '') {alert('paramètre vide !\nMerci d\'entrer un paramètre.');return false;}
+		
 		var data = {
 					type : 'enregistrer',
 					table : table,
@@ -150,13 +281,106 @@
 			type :"POST",
 			url : siteurl+'/includes/requeteenregistrenouveauparametre.php',
 			data : data,
-			success: function(result){
-				alert("Nouveau paramètre enregistré sous le n°"+result.result);				
+			success: function(result){console.log(result);
+				if (result.result == -1) {
+					alert("Ce paramètre existe déjà sous le n°"+result.id);
+				} else {
+					alert("Nouveau paramètre enregistré sous le n°"+result.result);
+				}
 			},
 			error: function(xhr) {console.log(xhr);alert('Erreur '+xhr.responseText);}
 		});
 	
 	});
+	
+	$(".modifier").click(function () {
+		var input = $(this).parent().prev('input');
+		var placeholder = input.attr('placeholder');
+		var select = $(this).parent().prev().prev();
+		if ((placeholder.lastIndexOf("AJOUTER") != -1) || ((placeholder.lastIndexOf("SUPPRIMER") != -1))){
+			select.removeAttr('disabled');
+			input.removeAttr('disabled');
+			input.val('');
+			placeholder = placeholder.replace("AJOUTER","MODIFIER");
+			placeholder = placeholder.replace("SUPPRIMER","MODIFIER");
+			input.attr('placeholder',placeholder);
+			return false;
+		}
+		var valeurs = select.val();
+		var modif = input.val();
+		var table = input.attr('id');
+		if (modif == '') {alert('paramètre vide !\nMerci d\'entrer un paramètre.');return false;}
+		
+		var data = {
+					type : 'modifier',
+					table : table,
+					valeurs : ''+valeurs+'',
+					modif : ''+modif+''
+				};
+		console.log(data);
+		$.ajax({
+			async : false,
+			type :"POST",
+			url : siteurl+'/includes/requeteenregistrenouveauparametre.php',
+			data : data,
+			success: function(result){
+				if (result.result == -1) {
+					alert(valeurs+" n'existe pas dans "+table);
+				} else if (result.result == 1) {
+					alert(valeurs+" modifié en "+modif+" dans "+table);
+				} else {
+					alert(valeurs+" supprimé.");
+				}
+			},
+			error: function(xhr) {console.log(xhr);alert('Erreur '+xhr.responseText);}
+		});
+	});
+	
+	$(".supprimer").click(function () {
+		var input = $(this).parent().prev('input');
+		var placeholder = input.attr('placeholder');
+		var select = $(this).parent().prev().prev();
+		if ((placeholder.lastIndexOf("MODIFIER") != -1) || ((placeholder.lastIndexOf("AJOUTER") != -1))){
+			select.removeAttr('disabled');
+			input.attr('disabled','disabled');
+			input.val('');
+			placeholder = placeholder.replace("MODIFIER","SUPPRIMER");
+			placeholder = placeholder.replace("AJOUTER","SUPPRIMER");
+			input.attr('placeholder',placeholder);
+			return false;
+		}
+		var valeurs = select.val();
+		var table = input.attr('id');
+		if (valeurs == '') {alert('paramètre vide !\nMerci d\'entrer un paramètre.');return false;}
+		
+		var data = {
+					type : 'supprimer',
+					table : table,
+					valeurs : ''+valeurs+''
+				};
+		console.log(data);
+		$.ajax({
+			async : false,
+			type :"POST",
+			url : siteurl+'/includes/requeteenregistrenouveauparametre.php',
+			data : data,
+			success: function(result){
+				if (result.result == -1) {
+					alert(valeurs+" n'existe pas dans "+table);
+				} else if (result.result == 0) {
+					var liste = '';
+					for (k in result.id) {
+						liste += '\n'+result.id[k];
+					}
+					alert("Pour pouvoir supprimer "+valeurs+" dans "+table+"\nIl faut d'abord modifier :"+liste);
+				} else {
+					alert(valeurs+" supprimé.");
+				}
+			},
+			error: function(xhr) {console.log(xhr);alert('Erreur '+xhr.responseText);}
+		});
+	
+	});	
 	
 	</script>
 
