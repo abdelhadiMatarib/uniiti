b6345952cf197d4615bf32f6c08afebef3e95f25
Selim - Maison <sfraoua@gmail.com>
2013-11-23 16:00:59 +0100

    Ajout de l'extraction des 10 derniers commentaires

--- includes/requeteminisite.php
+++ includes/requeteminisite.php
@@ -25,7 +25,7 @@
 	$data['enseigne'] = $result;
 
 	// On récupère les informations sur le gérant
-	$sql2 = "SELECT * FROM contributeurs
+	/* $sql2 = "SELECT * FROM contributeurs
 			WHERE id_contributeur = :id_contributeur
 		";
 	$req2 = $bdd->prepare($sql2);
@@ -33,9 +33,11 @@
 	$req2->execute();
 	$result2 = $req2->fetch(PDO::FETCH_ASSOC);
 	$data['gerant'] = $result2;
+         
+         */
 
 	// On récupère les informations sur les avis
-	$sql3 = "SELECT COUNT(id_avis) AS count_avis, AVG(note) AS moyenne
+	$sql3 = "SELECT COUNT(id_avis) AS count_avis, AVG(note) AS moyenne, commentaire
 			FROM avis AS t1
 
 			INNER JOIN enseignes_recoient_avis AS t2
@@ -48,15 +50,43 @@
 						ON t4.contributeurs_id_contributeur = t5.id_contributeur
 			WHERE id_enseigne = :id_enseigne
 			";
+	
 	$req3 = $bdd->prepare($sql3);
 	$req3->bindParam(':id_enseigne', $id_enseigne, PDO::PARAM_INT);
 	$req3->execute();
 	$result3 = $req3->fetch(PDO::FETCH_ASSOC);
+        
+        // On récupère les informations sur les avis
+	$sqlAvis = "SELECT commentaire, note, date_avis, t5.prenom_contributeur, LEFT(t5.nom_contributeur, 1) as nom_contributeur
+			FROM avis AS t1
+
+			INNER JOIN enseignes_recoient_avis AS t2
+			ON t1.id_avis = t2.avis_id_avis
+			INNER JOIN enseignes AS t3
+				ON t2.enseignes_id_enseigne = t3.id_enseigne
+				INNER JOIN contributeurs_donnent_avis AS t4
+					ON t1.id_avis = t4.avis_id_avis
+					INNER JOIN contributeurs AS t5
+						ON t4.contributeurs_id_contributeur = t5.id_contributeur
+			WHERE id_enseigne = :id_enseigne
+                                AND id_statut = 2
+                                AND commentaire != '' 
+                        ORDER BY date_avis DESC
+                        LIMIT 10
+			";
+        $reqAvis = $bdd->prepare($sqlAvis);
+	$reqAvis->bindParam(':id_enseigne', $id_enseigne, PDO::PARAM_INT);
+	$reqAvis->execute();
+	$resultAvis = $reqAvis->fetchAll(PDO::FETCH_ASSOC);
+        
+        $data['avis'] = $resultAvis;
+        
 	$data['enseigne']['nombre_avis'] = $result3['count_avis'];
 	$data['enseigne']['note'] = number_format($result3['moyenne'],1);
 	
 	// On récupère les informations sur les abonnés
-	$sql4 = "SELECT COUNT(contributeurs_id_contributeur) AS count_abonnes
+	/*
+        $sql4 = "SELECT COUNT(contributeurs_id_contributeur) AS count_abonnes
 			FROM contributeurs_follow_enseignes AS t1
 			WHERE enseignes_id_enseigne = :id_enseigne
 			";
@@ -65,9 +95,11 @@
 	$req4->execute();
 	$result4 = $req4->fetch(PDO::FETCH_ASSOC);
 	$data['enseigne']['nombre_abonnes'] = $result4['count_abonnes'];
-	
+	*/
+        
 	// On compte le nombre de commerce en réseau avec l'enseigne	
-	$sql5 = "SELECT COUNT(enseignes_id_enseigne1) AS count_reseau
+	/*
+        $sql5 = "SELECT COUNT(enseignes_id_enseigne1) AS count_reseau
 			FROM enseignes_reseau_enseignes AS t1
 			WHERE (enseignes_id_enseigne1 = :id_enseigne OR enseignes_id_enseigne2 = :id_enseigne) AND id_statut = 2
 			";
@@ -76,7 +108,8 @@
 	$req5->execute();
 	$result5 = $req5->fetch(PDO::FETCH_ASSOC);
 	$data['enseigne']['nombre_reseau'] = $result5['count_reseau'];
-	
+	*/
+        
 	// Labels et recommandations
 	$sql6 = "SELECT * FROM enseignes_labelsuniiti AS t1
 				INNER JOIN labelsuniiti AS t2
@@ -99,13 +132,17 @@
 	$data['enseigne']['recommandations'] = $result7;		
 
 	// Mots clés
-	$sql8 = "SELECT id_type_info, id_motcle1, id_motcle2, id_motcle3 FROM enseignes_infos_generales WHERE enseignes_id_enseigne=" . $id_enseigne;
+	/*$sql8 = "SELECT id_type_info, id_motcle1, id_motcle2, id_motcle3 FROM enseignes_infos_generales WHERE enseignes_id_enseigne=" . $id_enseigne;
 	$req8 = $bdd->prepare($sql8);
 	$req8->execute();
-	$result8 = $req8->fetchAll(PDO::FETCH_ASSOC);
-	$sql9 = "SELECT motcle FROM motscles WHERE id_motcle=:id_motcle";
+	$result8 = $req8->fetchAll(PDO::FETCH_ASSOC); 
+         */
+	/*
+         * $sql9 = "SELECT motcle FROM motscles WHERE id_motcle=:id_motcle";
+         *
 	$req9 = $bdd->prepare($sql9);
 	$AfficheMotcle[1] = $AfficheMotcle[2] = $AfficheMotcle[3] = $AfficheMotcle[4] = false;
+        $MotCle = null;
 	foreach ($result8 as $row8) {
 		$AfficheMotcle[$row8['id_type_info']] = true;
 		$req9->bindParam(':id_motcle', $row8['id_motcle1'], PDO::PARAM_INT);
@@ -121,9 +158,11 @@
 		$result9 = $req9->fetch(PDO::FETCH_ASSOC);
 		$MotCle[$row8['id_type_info']][3] = $result9['motcle'];			
 	}
+        //print_r($MotCle);
 	$data['enseigne']['motscles'] = $MotCle;
-	
-	$sql10 = "SELECT t1.prestation, t2.contenu, t2.prix FROM enseignes_prestations AS t1
+	*/
+	/*
+        $sql10 = "SELECT t1.prestation, t2.contenu, t2.prix FROM enseignes_prestations AS t1
 				INNER JOIN enseignes_prestations_contenus AS t2
 				ON t1.enseignes_id_enseigne = t2.enseignes_id_enseigne AND t1.id_type_info = t2.id_type_info
 				WHERE t1.enseignes_id_enseigne=:id_enseigne";
@@ -132,7 +171,8 @@
 	$req10->execute();
 	$result10 = $req10->fetchAll(PDO::FETCH_ASSOC);
 	$data['enseigne']['prestations'] = $result10;
-					
+		*/
+        /*
 	$sql11 = "SELECT COUNT(contributeurs_id_contributeur) AS count_aime
 				FROM contributeurs_aiment_enseignes AS t1
 					WHERE enseignes_id_enseigne = :id_enseigne";
@@ -141,7 +181,8 @@
 	$req11->execute();
 	$result11 = $req11->fetch(PDO::FETCH_ASSOC);
 	$data['enseigne']['nombre_aime'] = $result11['count_aime'];
-
+*/
+        /*
 	$sql12 = "SELECT COUNT(contributeurs_id_contributeur) AS count_aime_pas
 				FROM contributeurs_aiment_pas_enseignes AS t1
 					WHERE enseignes_id_enseigne = :id_enseigne";
@@ -150,7 +191,8 @@
 	$req12->execute();
 	$result12 = $req12->fetch(PDO::FETCH_ASSOC);
 	$data['enseigne']['nombre_aime_pas'] = $result12['count_aime_pas'];
-
+*/
+        /*
 	$sql13 = "SELECT COUNT(contributeurs_id_contributeur) AS count_wish
 				FROM contributeurs_wish_enseignes AS t1
 					WHERE enseignes_id_enseigne = :id_enseigne";
@@ -159,9 +201,10 @@
 	$req13->execute();
 	$result13 = $req13->fetch(PDO::FETCH_ASSOC);
 	$data['enseigne']['nombre_wish'] = $result13['count_wish'];
-	
+	*/
+        /*
 	$req->closeCursor();
-	$req2->closeCursor();
+	//$req2->closeCursor();
 	$req3->closeCursor();
 	$req4->closeCursor();
 	$req5->closeCursor();
@@ -173,6 +216,7 @@
 	$req11->closeCursor();
 	$req12->closeCursor();
 	$req13->closeCursor();
-	
-	echo json_encode($data);
+	*/
+        echo json_encode($data);
+        //echo json_encode($data);
 ?>
